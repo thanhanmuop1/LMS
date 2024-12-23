c9ccdf47770a076d6b8f798d86b5f5607a3e1093
hoahoang4205 <HoaHT.B23CC069@stu.ptit.edu.vn>
2024-12-24 03:12:46 +0700

    css

--- project web1/backend/src/configs/database.js
+++ project web1/backend/src/configs/database.js
@@ -3,7 +3,7 @@ const mysql = require('mysql2');
 const db = mysql.createConnection({
     host: '127.0.0.1',
     user: 'root',
-    password: '',
+    password: 'hoađôđăđ̀Â',
     database: 'lms'
 });
 
